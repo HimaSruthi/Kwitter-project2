242378002b2f1439a92a0bae644c4cf64e432044
HimaSruthi <106444449+HimaSruthi@users.noreply.github.com>
2022-06-13 12:58:02 -0400

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBbp8K_kSByB4GL2PBR_qj2hTU-hZ2InaQ",
+    authDomain: "kwitter-project-fa2bb.firebaseapp.com",
+    projectId: "kwitter-project-fa2bb",
+    storageBucket: "kwitter-project-fa2bb.appspot.com",
+    messagingSenderId: "348018415436",
+    appId: "1:348018415436:web:a675502befb2fa6f06e1e9"
+  };
+  
+  // Initialize Firebase
+  
