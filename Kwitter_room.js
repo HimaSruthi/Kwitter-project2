298c7d255986823f117139cc68d86f1bcaa8c7d7
HimaSruthi <106444449+HimaSruthi@users.noreply.github.com>
2022-07-20 13:08:58 -0400

    Add files via upload

--- Kwitter_room.js
+++ Kwitter_room.js
@@ -1,12 +1,47 @@
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBbp8K_kSByB4GL2PBR_qj2hTU-hZ2InaQ",
-    authDomain: "kwitter-project-fa2bb.firebaseapp.com",
-    projectId: "kwitter-project-fa2bb",
-    storageBucket: "kwitter-project-fa2bb.appspot.com",
-    messagingSenderId: "348018415436",
-    appId: "1:348018415436:web:a675502befb2fa6f06e1e9"
-  };
+  apiKey: "AIzaSyBI6UTYWCSfM22JzU_sAL-2wh4kum14Y2M",
+  authDomain: "kwitter-project-3-9f681.firebaseapp.com",
+  projectId: "kwitter-project-3-9f681",
+  storageBucket: "kwitter-project-3-9f681.appspot.com",
+  messagingSenderId: "668335438461",
+  appId: "1:668335438461:web:729f0d03262da84d2acf14"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
   
-  // Initialize Firebase
   
+  function addRoom (){
+    room_name=document.getElementById("room_name").value;
+    firebase.database().ref("/").child("room_name").update({
+          purpose:"adding room name"
+    });
+    localStorage.setitem("room_name",room_name);
+    window.location="kwitter_page.html";
+
+
+}
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+     Room_names = childKey;
+    //Start code
+    console.log("Room Name-"+Room_names);
+    row="<div class='room_name' id="+Room_names+" onclick='rediractToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+    document.getElementById("output").innerHTML+=row;
+    
+    //End code
+    });});}
+getData();
+function rediractToRoomName(name){
+    console.log(name);
+    localStorage.setitem("room_name",name);
+    window.location="kwitter_page.html";
+}
+
+function logout(){
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+  window.location="kwitter.html";
+  
+}
